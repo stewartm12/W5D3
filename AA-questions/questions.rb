6d52be485ba7b7de91ca1c5c76f1e6f50836e83c
stewart morales <stewartmorales129@gmail.com>
2021-01-27 05:18:09 -0500

    Finish setting up questions.rb

--- AA-questions/questions.rb
+++ AA-questions/questions.rb
@@ -13,15 +13,100 @@ end
 
 
 class Question
+  attr_accessors :id, :title, :body, :user_id
+
   def self.find_by_id(id)
-    question = QuestionDBConnection.instance.execute(<<-SQL, id)
+    option = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT *
       FROM questions
       WHERE id = ?
     SQL
-    Question.new(question)
+    Question.new(option)
   end
 
-  
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @user_id = options['user_id']
+  end
 
+end
+
+class User
+  attr_accessors :id, :fname, :lname
+
+  def self.find_by_id(id)
+    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM users
+      WHERE id = ?
+    SQL
+    User.new(option)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end
+
+class QuestionFollow
+  attr_accessors :id, :user_id, :question_id
+
+  def self.find_by_id(id)
+    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM question_follows
+      WHERE id = ?
+    SQL
+    QuestionFollow.new(option)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+end
+
+class Reply
+  attr_accessors :id, :body, :parent_id, :user_id, :question_id
+
+  def self.find_by_id(id)
+    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM replies
+      WHERE id = ?
+    SQL
+    Reply.new(option)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @body = options['body']
+    @parent_id = options['parent_id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end 
+end
+
+class QuestionLike
+  attr_accessors :id, :user_id, :question_id
+
+  def self.find_by_id(id)
+    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM question_likes
+      WHERE id = ?
+    SQL
+    QuestionLike.new(option)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
 end
