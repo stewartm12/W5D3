8044b8438805ec8f1b7bcb7321e98acf1aeb67bd
Kevin <kevinmao.xy@gmail.com>
2021-01-27 11:42:23 -0500

    create all tables in questions schema

--- AA-questions/import_db.sql
+++ AA-questions/import_db.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users;
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
@@ -35,3 +35,34 @@ CREATE TABLE replies (
   FOREIGN KEY(question_id) REFERENCES questions(id),
   FOREIGN KEY(parent_id) REFERENCES replies(id)
 );
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  question_id INTEGER,
+
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(question_id) REFERENCES questions(id)
+);
+
+INSERT INTO users 
+  (fname, lname)
+VALUES
+  ("Stewart", "Morales"),
+  ("Kevin", "Mao");
+
+INSERT INTO questions 
+  (title, body, user_id)
+VALUES
+  ("SM question", "What is a sun?", 
+  (
+    SELECT id
+    FROM users
+    WHERE fname = "Stewart" AND lname = 'Morales'
+  )),
+  ("KM question", "What is a moon?",
+  (
+    SELECT id
+    FROM users
+    WHERE fname = "Kevin" AND lname = 'Mao'
+  ));
