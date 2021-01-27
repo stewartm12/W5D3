29645af6299f6cec152b3fff4ad78b07888e4b25
Kevin <kevinmao.xy@gmail.com>
2021-01-27 14:10:36 -0500

    modify return data structure to array for class method queries

--- AA-questions/questions.rb
+++ AA-questions/questions.rb
@@ -13,15 +13,24 @@ end
 
 
 class Question
-  attr_accessors :id, :title, :body, :user_id
+  attr_accessor :id, :title, :body, :user_id
 
   def self.find_by_id(id)
-    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT *
       FROM questions
       WHERE id = ?
     SQL
-    Question.new(option)
+    data.map{ |datum| Question.new(datum)} 
+  end
+
+  def self.find_by_author_id(author_id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, author_id)
+      SELECT *
+      FROM questions
+      WHERE user_id = ?
+    SQL
+    data.map{ |datum| Question.new(datum)} 
   end
 
   def initialize(options)
@@ -34,15 +43,24 @@ class Question
 end
 
 class User
-  attr_accessors :id, :fname, :lname
+  attr_accessor :id, :fname, :lname
 
   def self.find_by_id(id)
-    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT *
       FROM users
       WHERE id = ?
     SQL
-    User.new(option)
+    data.map{|datum| User.new(datum)} 
+  end
+
+  def self.find_by_name(fname, lname)
+    data = QuestionDBConnection.instance.execute(<<-SQL, fname, lname)
+      SELECT *
+      FROM users
+      WHERE fname = ? AND lname = ?
+    SQL
+    data.map{|datum| User.new(datum)} 
   end
 
   def initialize(options)
@@ -50,18 +68,22 @@ class User
     @fname = options['fname']
     @lname = options['lname']
   end
+
+  def authored_questions
+    Question::find_by_author_id(id)
+  end
 end
 
 class QuestionFollow
-  attr_accessors :id, :user_id, :question_id
+  attr_accessor :id, :user_id, :question_id
 
   def self.find_by_id(id)
-    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT *
       FROM question_follows
       WHERE id = ?
     SQL
-    QuestionFollow.new(option)
+    data.map{|datum| QuestionFollow.new(datum)} 
   end
 
   def initialize(options)
@@ -72,15 +94,33 @@ class QuestionFollow
 end
 
 class Reply
-  attr_accessors :id, :body, :parent_id, :user_id, :question_id
+  attr_accessor :id, :body, :parent_id, :user_id, :question_id
 
   def self.find_by_id(id)
-    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT *
       FROM replies
       WHERE id = ?
     SQL
-    Reply.new(option)
+    data.map{|datum| Reply.new(datum)} 
+  end
+
+  def self.find_by_user_id(user_id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, user_id)
+      SELECT *
+      FROM replies
+      WHERE user_id = ?
+    SQL
+    data.map{|datum| Reply.new(datum)} 
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, question_id)
+      SELECT *
+      FROM replies
+      WHERE question_id = ?
+    SQL
+    data.map{|datum| Reply.new(datum)} 
   end
 
   def initialize(options)
@@ -93,15 +133,15 @@ class Reply
 end
 
 class QuestionLike
-  attr_accessors :id, :user_id, :question_id
+  attr_accessor :id, :user_id, :question_id
 
   def self.find_by_id(id)
-    option = QuestionDBConnection.instance.execute(<<-SQL, id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
       SELECT *
       FROM question_likes
       WHERE id = ?
     SQL
-    QuestionLike.new(option)
+    data.map{|datum| QuestionLike.new(datum)} 
   end
 
   def initialize(options)
