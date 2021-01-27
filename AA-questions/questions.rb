940548f949a72673abc887c60a7eba58009f1547
Kevin <kevinmao.xy@gmail.com>
2021-01-27 13:32:34 -0500

    make Question::find_by_id

--- AA-questions/questions.rb
+++ AA-questions/questions.rb
@@ -1,13 +1,25 @@
 require 'sqlite3'
 require 'singleton'
 
-class QuestionsDBConnection < SQLite3::Database
+class QuestionDBConnection < SQLite3::Database
   include Singleton
 
   def initialize
     super('questions.db')
     self.type_translation = true
     self.results_as_hash = true
+  end
+end
+
+
+class Question
+  def self.find_by_id(id)
+    question = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM questions
+      WHERE id = ?
+    SQL
+    Question.new(question)
   end
 
   
